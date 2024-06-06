a0f4c857ee0f289ed97fce32210237caf8969ebe
あさり <136340019+isshi0306@users.noreply.github.com>
2024-06-06 12:25:41 +0900

    x.comでのリダイレクトに関する修正

--- background.js
+++ background.js
@@ -6,14 +6,34 @@ chrome.declarativeNetRequest.updateDynamicRules({
             action: {
                 type: "redirect",
                 redirect: {
-                    regexSubstitution: "https://twitter.com?mx=1"
+                    regexSubstitution: "https://twitter.com\\1"
                 }
             },
             condition: {
-                regexFilter: "^https://(www\\.)?(twitter\\.com|x\\.com)/.*",
+                regexFilter: "^https://(www\\.)?x\\.com(/.*)?",
+                resourceTypes: ["main_frame"]
+            }
+        },
+        {
+            id: 2,
+            priority: 1,
+            action: {
+                type: "redirect",
+                redirect: {
+                    transform: {
+                        queryTransform: {
+                            addOrReplaceParams: [
+                                { key: "mx", value: "1" }
+                            ]
+                        }
+                    }
+                }
+            },
+            condition: {
+                regexFilter: "^https://(www\\.)?twitter\\.com/.*",
                 resourceTypes: ["main_frame"]
             }
         }
     ],
-    removeRuleIds: [1]
+    removeRuleIds: [1, 2]
 });
